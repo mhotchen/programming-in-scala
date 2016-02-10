567612168820f1949846e2fb3613ed3acbfd98ac
Matthew Hotchen <github@mhn.me>
2016-02-10 19:55:05 +0000

    Use placeholders and partially applied functions
    This makes the code more concise and focuses on the problem to solve
    rather than syntax

--- src/LongLines.scala
+++ src/LongLines.scala
@@ -2,20 +2,16 @@ import scala.io.Source
 
 class LongLines {
   def processFile(fileName: String, width: Int): Unit = {
-    def filterLine(line: String) = if (line.length >= width) true else false
-    def printLine(line: String) = println(fileName + ":" + line.length + ": " + line)
-
     Source.fromFile(fileName)
       .getLines
-      .filter(filterLine)
-      .foreach(printLine)
+      .filter(_.length >= width)
+      .foreach(line => println(fileName + ":" + line.length + ": " + line))
   }
 }
 
 object LongLines {
   def main(args: Array[String]): Unit = {
-    val width = args(0).toInt
-    val longLines = new LongLines
-    for (arg <- args.drop(1)) longLines.processFile(arg, width)
+    val processFile = (new LongLines).processFile(_: String, args(0).toInt)
+    args drop 1 foreach processFile
   }
 }
