cf9a745a02d30c7cab3ef430849a4aafae6ecf1e
Matthew Hotchen <github@mhn.me>
2016-05-23 08:48:46 +0100

    Delete expr to reimplement
    Stopped learning scala for a while so going back
    a few chapters to get familar with the constructs

--- src/expr/Expr.scala
+++
@@ -1,7 +0,0 @@
-package expr
-
-sealed trait Expr
-case class Var(name: String) extends Expr
-case class Number(num: Double) extends Expr
-case class UnOp(operator: String, arg: Expr) extends Expr
-case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
